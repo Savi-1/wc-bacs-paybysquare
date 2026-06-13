2f92c779751e2db312f1b7fc66b8938a9c46c981
Peter Nemcok <peter.nemcok@gmail.com>
2026-06-13 19:33:14 +0200

    chore: declare WP "Tested up to" 6.9.4

--- wc-bacs-paybysquare.php
+++ wc-bacs-paybysquare.php
@@ -17,6 +17,7 @@
  * Text Domain: wc-bacs-paybysquare
  * Domain Path: /languages
  * Requires at least: 6.0
+ * Tested up to: 6.9.4
  * Requires PHP: 7.4
  * WC requires at least: 8.0
  * WC tested up to: 10.6
